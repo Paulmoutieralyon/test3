db0faefb53ec9e80a31eee5cb89c24203ccd3359
Paul Moutier[LYON] <moutier.paul@gmail.com>
2018-09-03 22:38:24 +0200

    Pseudo-code4

---
+++ javas.js
@@ -0,0 +1,16 @@
+console.log(" debut du Programme")
+var nombre = Number(prompt("Inscrivez un nombre"))
+alert("Bienvenue A la Wild Code School !")
+var bienvenue = "Bienvenue a l'ecolde Wild code "
+
+
+while(nombre<400)
+    {
+        console.log(bienvenue);
+        nombre++;
+    }
+    
+    
+
+console.log(" Fin du Programme ")
+    
